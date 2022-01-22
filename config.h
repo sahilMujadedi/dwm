5cf6a553ab66dcc0fad68df1ac361b85b06a9a1b
sahilMujadedi <mujadedisahil@gmail.com>
2022-01-21 23:25:59 -0700

    changes

--- config.h
+++ config.h
@@ -61,8 +61,15 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "urxvt", NULL };
 
+static const char *upvol[] =   { "amixer", "set", "Master", "3+",     NULL };
+static const char *downvol[] = { "amixer", "set", "Master", "3-",     NULL };
+static const char *mutevol[] = { "amixer", "set", "Master", "toggle", NULL };
+
 static Key keys[] = {
 	/* modifier                     key        function        argument */
+	{ MODKEY,                       XK_F12,    spawn,          {.v = upvol    } },
+	{ MODKEY,                       XK_F11,    spawn,          {.v = downvol  } },
+	{ MODKEY,                       XK_F9,     spawn,          {.v = mutevol  } },
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
